51ef5a27b1a843d30ca75a4c9c2e7f12c974e3d5
aaadev32 <aaadev32@gmail.com>
2022-02-07 11:03:22 -0800

    exercise 3 finish

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
-};
+const reverseString = function (string) {
+    let reversal = [];
+    let result = "";
+    let length = string.length - 1;
+    //loops through string input starting from the string length effectively creating a reversed string when iterated from 0 to string length
+    for (i = length; i >= 0; i--) {
+        reversal[i] = string[i];
+        result += reversal[i];
+    }
+  return result;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
