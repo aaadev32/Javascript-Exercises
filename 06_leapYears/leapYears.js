5073e71987772c9a8884e469544e3409667471f5
aaadev32 <aaadev32@gmail.com>
2022-02-09 09:53:25 -0800

    exercise 6 done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,18 @@
-const leapYears = function() {
+const leapYears = function (year) {
+
+    let x = "";
+
+    if (year % 400 == 0) { // 1900 % 4 = 0 but year % 100 needs to return false
+        x = 0;
+    } else if (year % 4 != 0 || year % 100 == 0) {
+        x = 1;
+    }
+
+    if (x == 0) {
+        return true;
+    } else if (x == 1) {
+        return false;
+    }
 
 };
 
