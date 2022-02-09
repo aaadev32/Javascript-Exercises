529ba4a471952151c4534368e8dd557688524bf5
aaadev32 <aaadev32@gmail.com>
2022-02-09 08:38:12 -0800

    exercise 5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function (sum1, sum2) {
+    //if else if will subtract the lower sum from the greater sum and use that as an iterator count for the loop and use the lower sum as the start point
+    let iteratedSum = 0;
+    let start = 0;
+    let counter = 0;
+
+    if (Number.isInteger(sum1) == false || Number.isInteger(sum2) == false) {
+        return "ERROR";
+    } else if (sum2 > sum1) {
+        counter = sum2 - sum1;
+        start = sum1;
+    } else if (sum1 > sum2) {
+        counter = sum1 - sum2;
+        start = sum2;
+    }
+
+    for (i = 0; i <= counter; i++) {
+        iteratedSum += start;
+        start++;
+    }
+
+    if (iteratedSum < 0) {
+        return "ERROR";
+    } else {
+        return iteratedSum;
+    }
+
 
 };
 
