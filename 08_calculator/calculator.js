291b2591d662b25d57eabccaa1c5abdba9d7bebd
aaadev32 <aaadev32@gmail.com>
2022-02-09 20:41:19 -0800

    chckpoin

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  let result = num1 + num2;
+
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  let result = num1 - num2;
+
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sum = 0;
+
+  for (i = 0; i < arr.length; i++) {
+    sum += arr[i];
+
+    return sum;
+
+  }
+}
+
+
+const multiply = function () {
+
 };
 
-const multiply = function() {
+const power = function () {
 
 };
 
-const power = function() {
-	
-};
+const factorial = function () {
 
-const factorial = function() {
-	
 };
 
 // Do not edit below this line
