2cdff11b6ecd1e4f274d114489c1c38b9889f012
aaadev32 <aaadev32@gmail.com>
2022-02-09 22:08:25 -0800

    exercise 9 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
-};
+    let string = str.toLowerCase().replace(/[/\s+'!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~']/g, "");
+    let len = string.length;
+    let x = true;
 
+    for (let i = 0; i < len; i++) {
+        if (string[i] != string[len -1 - i]) {
+            x = false;
+        }
+    }
+    return x;
+}
 // Do not edit below this line
 module.exports = palindromes;
