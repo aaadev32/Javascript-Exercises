9b4087600dacf44fec169c804c31e459dc3e5584
aaadev32 <aaadev32@gmail.com>
2022-02-11 12:59:34 -0800

    exercise 11 done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+    let result = [];
 
+    for (i = 0; i < books.length; i++){
+        result.push(books[i].title);
+    }
+    return result;
 };
 
 // Do not edit below this line
