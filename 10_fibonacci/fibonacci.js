158f4ef059b034720e9c4ec0e786823d7968a4db
aaadev32 <aaadev32@gmail.com>
2022-02-22 10:13:04 -0800

    fibonacci finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(fib) {
+    let arr = [1,1];
+    let sum = 0;
+    let j = 1;
+    let i = 0;
+
+    if(fib < 0){
+        return "OOPS";
+    }
+
+    while(i < fib){
+
+        sum = arr[i] + arr[j];
+        arr.push(sum);
+        i++
+        j++
+        //console.log(arr)
+    }
+    return arr[fib - 1];
 
 };
 
