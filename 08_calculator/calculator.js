1a3bc9aca95d1c8310c347ff71610a500dd073fc
rusterz <aaadev32@gmail.com>
2022-03-15 10:19:55 -0700

    finished calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -2,19 +2,31 @@ const add = function (num1, num2) {
   let result = num1 + num2;
 
   return result;
+
 };
 
 const subtract = function (num1, num2) {
   let result = num1 - num2;
 
   return result;
+
 };
 
 const sum = function (arr) {
+
   let sum = 0;
 
-  for (i = 0; i < arr.length; i++) {
-    sum += arr[i];
+  if(arr.length <= 0){
+
+      return sum;
+
+  }else{
+
+    for (i = 0; i < arr.length; i++) {
+
+      sum += arr[i];
+
+    }
 
     return sum;
 
@@ -22,15 +34,52 @@ const sum = function (arr) {
 }
 
 
-const multiply = function () {
+const multiply = function (arr) {
 
+  let product = arr[0];
+
+  if(arr.length <= 0){
+
+      return product;
+
+  }else{
+
+    for (i = 1; i < arr.length; i++) {
+
+      product *= arr[i];
+
+    }
+
+    return product;
+
+  }
 };
 
-const power = function () {
+const power = function (int1, int2) {
 
+  let pow = Math.pow(int1, int2);
+
+  return pow;console.log(temp);
 };
 
-const factorial = function () {
+const factorial = function (int) {
+
+  let result = 1;
+  let temp = [];
+
+  if(int == 0){
+
+    return 1;
+
+  }
+
+  for(let i = 1; i <= int; i++){
+
+    result = result *= i;
+
+  }
+
+  return result;
 
 };
 
